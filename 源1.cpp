a7a0d329b9fc2c9a985f018a99b3f348d2cd7f00
ciec20151104681 <973636353@qq.com>
2016-06-29 11:57:41 +0800

    Add files via upload

---
+++ 源1.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main()
+{
+	char str[80];
+	int a;
+	int i=1;
+	char b,c[100];
+    FILE * fp1 = fopen("export.egx","r");//打开输入文件
+	if (fp1==NULL)
+	{
+		printf("不能打开文件!");
+		exit(1);
+	}
+	printf("能打开文件!\n");
+	/*rewind(fp1);
+	fseek(fp1,100l,0);
+	fsconf(fp1,);
+	printf()*/
+	fgets(str,500,fp1);
+	printf("%s",str);
+	fclose(fp1);
+
+	return 0;
+}
