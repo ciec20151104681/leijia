52be89abfb8954992576320a60f62608d4c0127e
ciec20151104681 <973636353@qq.com>
2016-07-11 11:48:52 +0800

    Add files via upload

---
+++ 未命名1.cpp
@@ -0,0 +1,12 @@
+#include"stdafx.h"
+#include"string.h"
+
+int _tmain(int argc,_TCHAR* argv[])
+{
+	char *ptr1="<?xml version=\1.0\" encodingUTF-8 standalone=no ?>
+	
+	char s[11];
+	int i=0;
+	while(!((*(ptrl+i)=='m')&&()))
+}
+
